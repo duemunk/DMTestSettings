8c38dcdc43b3bcc69aaeff913df16e268db736a4
Tobias Due Munk <tobiasdm@gmail.com>
2013-12-28 20:33:41 +0100

    Implement customizability for GridOverlay plugin

--- Test Settings/Default Plugins/DMGridOverlayPlugin.m
+++ Test Settings/Default Plugins/DMGridOverlayPlugin.m
@@ -83,7 +83,38 @@
 	[self setNeedsDisplay];
 }
 
+- (void)setVerticalSpacing:(NSUInteger)verticalSpacing
+{
+	if (verticalSpacing != _verticalSpacing) {
+		_verticalSpacing = verticalSpacing;
+		
+		[self setNeedsDisplay];
+	}
+}
+- (void)setHorizontalSpacing:(NSUInteger)horizontalSpacing
+{
+	if (horizontalSpacing != _horizontalSpacing) {
+		_horizontalSpacing = horizontalSpacing;
+		
+		[self setNeedsDisplay];
+	}
+}
+- (void)setLineWidth:(NSUInteger)lineWidth
+{
+	if (lineWidth != _lineWidth) {
+		_lineWidth = lineWidth;
+		
+		[self setNeedsDisplay];
+	}
+}
+
 @end
+
+
+
+
+
+
 
 
 
@@ -93,6 +124,9 @@
 #import "DMGridOverlayPlugin.h"
 
 @implementation DMGridOverlayPlugin
+{
+	UITableView *_tableView;
+}
 
 
 #define cellIdentifier @"GridOverlayCellIdentifier"
@@ -113,11 +147,11 @@
 							   kLineColor			:	[UIColor grayColor]};
 	
 	UITableViewController *tableViewController = [UITableViewController new];
-	UITableView *tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStyleGrouped];
-	tableViewController.tableView = tableView;
+	_tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStyleGrouped];
+	tableViewController.tableView = _tableView;
 	self.viewController = tableViewController;
 	
-	[tableViewController.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:cellIdentifier];
+	[tableViewController.tableView registerClass:[DMTableViewCell_StyleValue2 class] forCellReuseIdentifier:cellIdentifier];
 	tableViewController.tableView.dataSource = self;
 	tableViewController.tableView.delegate = self;
 }
@@ -143,7 +177,7 @@
 
 - (NSString *)settingsDescription
 {
-	return [NSString stringWithFormat:@"%d, %d",[[[DMTestSettings sharedInstance] objectForKey:kVerticalSpacing withPluginIdentifier:self.uniqueID] intValue],[[[DMTestSettings sharedInstance] objectForKey:kHorizontalSpacing withPluginIdentifier:self.uniqueID] intValue]];
+	return [NSString stringWithFormat:@"h:%d v:%d px",[[[DMTestSettings sharedInstance] objectForKey:kVerticalSpacing withPluginIdentifier:self.uniqueID] intValue],[[[DMTestSettings sharedInstance] objectForKey:kHorizontalSpacing withPluginIdentifier:self.uniqueID] intValue]];
 }
 
 
@@ -151,13 +185,14 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-	return 1;
+	return 2;
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
 {
 	switch (section) {
-		case 0: return @"Parameters";
+		case 0: return @"Spacing";
+		case 1: return @"Line";
 		default: return nil;
 	}
 }
@@ -165,42 +200,205 @@
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
 	switch (section) {
-		case 0: return 4;
+		case 0: return 2;
+		case 1: return 2;
 		default: return 0;
 	}
 }
 
 #pragma mark - UITableViewDelegate
 
+
+#define stepperViewTag 218734619
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
 	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
-	cell.accessoryView = nil;
-	cell.selectionStyle = UITableViewCellSelectionStyleDefault;
-	
-	switch (indexPath.section) {
-		case 0:
-		{
-			
-		}
-			break;
-		case 1:
-		{
-			
-		}
-			break;
-			
-		default:
-			break;
-	}
-	
-	
+	[self configureCell:cell forIndexPath:indexPath];
 	return cell;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
 	
+}
+
+
+#pragma mark -
+
+- (void)configureCell:(UITableViewCell *)cell forIndexPath:(NSIndexPath *)indexPath
+{
+	cell.accessoryType = UITableViewCellAccessoryNone;
+	cell.selectionStyle = UITableViewCellSelectionStyleDefault;
+	
+	cell.textLabel.textColor = [UIColor grayColor];
+	
+	switch (indexPath.section) {
+		case 0:
+		{
+			switch (indexPath.row) {
+				case 0:
+				{
+					cell.textLabel.text = @"Horizontal";
+					
+					int spacing = [[[DMTestSettings sharedInstance] objectForKey:kHorizontalSpacing withPluginIdentifier:self.uniqueID] intValue];
+					cell.detailTextLabel.text = [NSString stringWithFormat:@"%d px",spacing];
+					
+					cell.selectionStyle = UITableViewCellSelectionStyleNone;
+					
+					UIStepper *stepper;
+					if (cell.accessoryView.tag == stepperViewTag)
+					{
+						stepper = (UIStepper *)cell.accessoryView;
+						[stepper removeTarget:self action:NULL forControlEvents:UIControlEventValueChanged];
+					}
+					else
+					{
+						stepper = [UIStepper new];
+						stepper.tag = stepperViewTag;
+						stepper.minimumValue = 2;
+						cell.accessoryView = stepper;
+					}
+					stepper.value = spacing;
+					[stepper addTarget:self action:@selector(horizontalStepperChanged:) forControlEvents:UIControlEventValueChanged];
+				}
+					break;
+				case 1:
+				{
+					cell.textLabel.text = @"Vertical";
+					
+					int spacing = [[[DMTestSettings sharedInstance] objectForKey:kVerticalSpacing withPluginIdentifier:self.uniqueID] intValue];
+					cell.detailTextLabel.text = [NSString stringWithFormat:@"%d px",spacing];
+					
+					cell.selectionStyle = UITableViewCellSelectionStyleNone;
+					
+					UIStepper *stepper;
+					if (cell.accessoryView.tag == stepperViewTag)
+					{
+						stepper = (UIStepper *)cell.accessoryView;
+						[stepper removeTarget:self action:NULL forControlEvents:UIControlEventValueChanged];
+					}
+					else
+					{
+						stepper = [UIStepper new];
+						stepper.tag = stepperViewTag;
+						stepper.minimumValue = 2;
+						cell.accessoryView = stepper;
+					}
+					stepper.value = spacing;
+					[stepper addTarget:self action:@selector(verticalStepperChanged:) forControlEvents:UIControlEventValueChanged];
+				}
+					break;
+					
+				default:
+					break;
+			}
+		}
+			break;
+		case 1:
+		{
+			switch (indexPath.row) {
+				case 0:
+				{
+					cell.textLabel.text = @"Width";
+					
+					int lineWidth = [[[DMTestSettings sharedInstance] objectForKey:kLineWidth withPluginIdentifier:self.uniqueID] intValue];
+					cell.detailTextLabel.text = [NSString stringWithFormat:@"%d px",lineWidth];
+					[cell.detailTextLabel sizeToFit];
+					
+					cell.selectionStyle = UITableViewCellSelectionStyleNone;
+					
+					UIStepper *stepper;
+					if (cell.accessoryView.tag == stepperViewTag)
+					{
+						stepper = (UIStepper *)cell.accessoryView;
+						[stepper removeTarget:self action:NULL forControlEvents:UIControlEventValueChanged];
+					}
+					else
+					{
+						stepper = [UIStepper new];
+						stepper.tag = stepperViewTag;
+						stepper.minimumValue = 1;
+						cell.accessoryView = stepper;
+					}
+					stepper.value = lineWidth;
+
+					[stepper addTarget:self action:@selector(lineWidthStepperChanged:) forControlEvents:UIControlEventValueChanged];
+				}
+					break;
+				case 1:
+				{
+					cell.accessoryView = nil;
+					
+					cell.textLabel.text = @"Color";
+				}
+					break;
+					
+				default:
+					break;
+			}
+		}
+			break;
+			
+		default:
+			break;
+	}
+}
+
+- (void)horizontalStepperChanged:(UIStepper *)stepper
+{
+	UITableViewCell *cell;
+	if ([stepper.superview isKindOfClass:[UITableViewCell class]]) {
+		cell = (UITableViewCell *)stepper.superview;
+	}
+	else if ([stepper.superview.superview isKindOfClass:[UITableViewCell class]]) {
+		cell = (UITableViewCell *)stepper.superview.superview;
+	}
+	if (cell)
+	{
+		int spacing = stepper.value;
+		[[DMTestSettings sharedInstance] setObject:@(spacing) forKey:kHorizontalSpacing withPluginIdentifier:self.uniqueID];
+		[GridOverlay sharedInstance].horizontalSpacing = spacing;
+		NSIndexPath *indexPath = [_tableView indexPathForCell:cell];
+		[self configureCell:cell forIndexPath:indexPath];
+	}
+}
+
+- (void)verticalStepperChanged:(UIStepper *)stepper
+{
+	UITableViewCell *cell;
+	if ([stepper.superview isKindOfClass:[UITableViewCell class]]) {
+		cell = (UITableViewCell *)stepper.superview;
+	}
+	else if ([stepper.superview.superview isKindOfClass:[UITableViewCell class]]) {
+		cell = (UITableViewCell *)stepper.superview.superview;
+	}
+	if (cell)
+	{
+		int spacing = stepper.value;
+		[[DMTestSettings sharedInstance] setObject:@(spacing) forKey:kVerticalSpacing withPluginIdentifier:self.uniqueID];
+		[GridOverlay sharedInstance].verticalSpacing = spacing;
+		NSIndexPath *indexPath = [_tableView indexPathForCell:cell];
+		[self configureCell:cell forIndexPath:indexPath];
+	}
+}
+
+- (void)lineWidthStepperChanged:(UIStepper *)stepper
+{
+	UITableViewCell *cell;
+	if ([stepper.superview isKindOfClass:[UITableViewCell class]]) {
+		cell = (UITableViewCell *)stepper.superview;
+	}
+	else if ([stepper.superview.superview isKindOfClass:[UITableViewCell class]]) {
+		cell = (UITableViewCell *)stepper.superview.superview;
+	}
+	if (cell)
+	{
+		int spacing = stepper.value;
+		[[DMTestSettings sharedInstance] setObject:@(spacing) forKey:kLineWidth withPluginIdentifier:self.uniqueID];
+		[GridOverlay sharedInstance].lineWidth = spacing;
+		NSIndexPath *indexPath = [_tableView indexPathForCell:cell];
+		[self configureCell:cell forIndexPath:indexPath];
+	}
 }
 
 
