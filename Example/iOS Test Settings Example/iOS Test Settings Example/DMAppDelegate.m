d277c615b7e6126ca3222a17568c7df2c84fdce7
Tobias Due Munk <tobiasdm@gmail.com>
2013-12-28 20:59:19 +0100

    Update UI of example app

--- Example/iOS Test Settings Example/iOS Test Settings Example/DMAppDelegate.m
+++ Example/iOS Test Settings Example/iOS Test Settings Example/DMAppDelegate.m
@@ -13,28 +13,59 @@
 #import "DMColorBlindPlugin.h"
 
 
+@interface ViewController : UIViewController
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+	// Setup view
+	UILabel *label = [UILabel new];
+	[self.view addSubview:label];
+	label.text = @"Shake app to open test settings \n\n or ⌃⌘Z in Simulator";
+	label.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+	label.textColor = [UIColor whiteColor];
+	label.numberOfLines = 0;
+	label.textAlignment = NSTextAlignmentCenter;
+	NSDictionary *views = NSDictionaryOfVariableBindings(label);
+	label.translatesAutoresizingMaskIntoConstraints = NO;
+	[self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-padding-[label]-padding-|"
+																								options:0
+																								metrics:@{@"padding":@(20)}
+																								  views:views]];
+	[self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-padding-[label]-padding-|"
+																								options:0
+																								metrics:@{@"padding":@(20)}
+																								  views:views]];
+	
+    self.view.backgroundColor = [UIColor cyanColor];
+}
+
+- (UIStatusBarStyle)preferredStatusBarStyle
+{
+	return UIStatusBarStyleLightContent;
+}
+
+@end
+
+
+
 @implementation DMAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     
-	self.window.rootViewController = [UIViewController new];
+	self.window.rootViewController = [ViewController new];
 	
-	// Setup view
-	UILabel *label = [UILabel new];
-	[self.window.rootViewController.view addSubview:label];
-	label.text = @"Shake app to open test settings";
-	[label sizeToFit];
-	label.center = self.window.center;
 	
-    self.window.backgroundColor = [UIColor cyanColor];
     [self.window makeKeyAndVisible];
 	
 	// Start DMTestSettings _after_ setting rootViewController 
 	[DMTestSettings startWithPlugins:@[[DMGridOverlayPlugin new],[DMColorBlindPlugin new]]];
 	
-	[DMTestSettings sharedInstance].hidden = NO;
+//	[DMTestSettings sharedInstance].hidden = NO;
 	
     return YES;
 }
